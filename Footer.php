79ccd91beef36210b97dccf1c9f3e772dc2e0ae7
Makaifox <yu.trindade@gmail.com>
2023-06-24 17:45:30 -0300

    first commit

---
+++ Footer.php
@@ -0,0 +1,10 @@
+<footer class="footer fixed-bottom">
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/logo.png" alt="Logo" class="footer-logo">
+        <p class="footer-year">© 2023</p>
+      </div>
+    </div>
+  </div>
+</footer>
